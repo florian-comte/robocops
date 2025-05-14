3232705e9487802c1af9adad9a256d3fb91a8a56
Florian COMTE <florian.comte@epfl.ch>
2025-05-14 11:46:42 +0200

    fix robocops_duplos

--- src/robocops_duplos/src/duplos_mapper.cpp
+++ src/robocops_duplos/src/duplos_mapper.cpp
@@ -41,3 +41,12 @@ void DuplosMapper::detectionsCallback(const depthai_ros_msgs::msg::SpatialDetect
                     P_world.x(), P_world.y(), P_world.z());
     }
 }
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<DuplosMapper>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}

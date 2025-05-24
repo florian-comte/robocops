9d178bd05321da20a6d83d4a44905be5ee2353a2
Florian COMTE <florian.comte@epfl.ch>
2025-05-24 03:13:44 +0200

    add print to test

--- src/robocops_control/hardware/arduino_comms.cpp
+++ src/robocops_control/hardware/arduino_comms.cpp
@@ -107,6 +107,8 @@ void ArduinoComms::send_command(int16_t maxon_left,
     cmd[4] |= ((unload_activate & 1) << 1);
     cmd[4] |= ((lift_authorize & 1) << 2);
 
+    std::cout << cmd[4] << std::endl;
+
     fd_set write_fds;
     FD_ZERO(&write_fds);
     FD_SET(serial_fd_, &write_fds);

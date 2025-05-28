d33c4a49744d777cbc715e34d3a3490b6eb0bf6f
Florian COMTE <florian.comte@epfl.ch>
2025-05-28 21:37:49 +0200

    update fake tf zone

--- src/robocops_duplos/src/fake_tf_zone_publisher.cpp
+++ src/robocops_duplos/src/fake_tf_zone_publisher.cpp
@@ -14,8 +14,6 @@ public:
     {
         zone_pub_ = this->create_publisher<std_msgs::msg::Int32>("/zone", 10);
 
-        tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-
         timer_ = this->create_wall_timer(1s, std::bind(&FakeTFAndZonePublisher::publish, this));
     }
 
@@ -30,27 +28,9 @@ private:
 
         // Cycle through zones 1-4
         zone_ = zone_ % 4 + 1;
-
-        // Publish static TF from "camera_frame" to "map"
-        geometry_msgs::msg::TransformStamped t;
-        t.header.stamp = this->get_clock()->now();
-        t.header.frame_id = "map";
-        t.child_frame_id = "camera_frame";
-
-        // Static or small movement
-        t.transform.translation.x = 0.0;
-        t.transform.translation.y = 0.0;
-        t.transform.translation.z = 0.0;
-        t.transform.rotation.x = 0.0;
-        t.transform.rotation.y = 0.0;
-        t.transform.rotation.z = 0.0;
-        t.transform.rotation.w = 0.0;
-
-        tf_broadcaster_->sendTransform(t);
     }
 
     rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr zone_pub_;
-    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
     rclcpp::TimerBase::SharedPtr timer_;
     int zone_;
 };

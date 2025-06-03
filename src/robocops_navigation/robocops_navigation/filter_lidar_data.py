bbd3d5627403ec72b6de203ab7e2f3500aa5b043
Florian COMTE <florian.comte@epfl.ch>
2025-06-03 23:14:00 +0200

    add import

--- src/robocops_navigation/robocops_navigation/filter_lidar_data.py
+++ src/robocops_navigation/robocops_navigation/filter_lidar_data.py
@@ -35,7 +35,7 @@ class ScanFilter(Node):
         # self.get_logger().info(f"Filtered scan: kept {kept_count} out of {num_points} points ({100 * kept_count / num_points:.1f}%)")
         
         msg.ranges = new_ranges
-        msg.header.stamp.sec = time()
+        msg.header.stamp.sec = time.time()
         msg.header.stamp.nsec = time.time_ns()
         self.pub.publish(msg)
 

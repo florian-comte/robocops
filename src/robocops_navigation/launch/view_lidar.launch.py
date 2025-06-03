8c99c07d806e6399bc815ea432b881ea3c0e3ee0
Florian COMTE <florian.comte@epfl.ch>
2025-06-03 20:39:11 +0200

    fix indent

--- src/robocops_navigation/launch/view_lidar.launch.py
+++ src/robocops_navigation/launch/view_lidar.launch.py
@@ -3,7 +3,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import PathJoinSubstitution
- from launch_ros.substitutions import FindPackageShare
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     ld = LaunchDescription()

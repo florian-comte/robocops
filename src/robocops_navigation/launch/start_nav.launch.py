eda9568b33fdfbadfd25e51a7a6fc7961ce3742a
Florian COMTE <florian.comte@epfl.ch>
2025-06-04 13:50:45 +0200

    change nav2

--- src/robocops_navigation/launch/start_nav.launch.py
+++ src/robocops_navigation/launch/start_nav.launch.py
@@ -6,6 +6,8 @@ from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
+from launch.actions import GroupAction
+from launch_ros.actions import PushRosNamespace
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -13,14 +15,32 @@ def generate_launch_description():
     nav2_yaml = os.path.join(get_package_share_directory('robocops_navigation'), 'config', 'nav2_params.yaml')
     map_file = os.path.join(get_package_share_directory('robocops_navigation'), 'maps', 'mapv1.yaml')
 
-    nav2_launch_file = IncludeLaunchDescription(
+    nav2_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('nav2_bringup'),
-                         'launch/bringup_launch.py')
+            os.path.join(
+                get_package_share_directory('nav2_bringup'),
+                'launch',
+                'bringup_launch.py'
+            )
         ),
-        launch_arguments={'slam': 'False', 'map': map_file, 'use_sim_time': 'False', 'params_file': nav2_yaml, 'autostart': 'True'}.items()
+        launch_arguments={
+            'slam': 'False',
+            'map': map_file,
+            'use_sim_time': 'False',
+            'params_file': nav2_yaml,
+            'autostart': 'True'
+        }.items()
     )
 
-    ld.add_action(nav2_launch_file)
+    nav2_with_remap = GroupAction(
+        actions=[
+            nav2_launch,
+        ],
+        remappings=[
+            ('/cmd_vel_nav', '/cmd_vel')
+        ]
+    )
+
+    ld.add_action(nav2_with_remap)
 
     return ld

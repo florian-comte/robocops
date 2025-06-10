5e97dc4223ea1e4a5c580b09b4df10a13736e602
Florian COMTE <florian.comte@epfl.ch>
2025-06-10 14:14:59 +0200

    fix client

--- client.py
+++ client.py
@@ -228,7 +228,7 @@ class DuploControl(Node):
         self.enable_capture(True)
         
 
-        self.send_navigation_goal(pos.x, pos.y)
+        self.send_navigation_goal(pos.x - 10, pos.y)
         
         time.sleep(5)
         
